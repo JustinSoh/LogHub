b4741a259751428706fad8c770ed848918798c4c
Unknown <hoggersoh@gmail.com>
2018-05-26 23:08:51 +0800

    Auto stash before merge of "master" and "origin/master"

--- log-hub/src/app/login/change-details/change-details.component.ts
+++ log-hub/src/app/login/change-details/change-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormControl, Validators} from '@angular/forms';
+import { md5 }  from 'node_modules/md5';
+import { LoginDetailsComponent} from '../login-details/login-details.component'
 @Component({
   selector: 'app-change-details',
   templateUrl: './change-details.component.html',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangeDetailsComponent implements OnInit {
 
+  password = new FormControl('', [Validators.required]);
+  retype = new FormControl('', [Validators.required]);
+  errorMessage = "";
+  passwordBoolean = false
   constructor() { }
-
+  hashedCode = new LoginDetailsComponent();
   ngOnInit() {
   }
-
-}
+  
+  changeDetails()
+  {
+    if(this.password.valid && this.retype.valid)
+    {
+      var hashpw = this.hashedCode.hashpw(this.password.value);
+      var hashre = this.hashedCode.hashpw(this.retype.value);
+      
+      if(hashpw == hashre)
+      {
+         this.passwordBoolean = true;  
+      }
+      else {
+        this.passwordBoolean = false;
+        this.errorMessage = "Password doesn't match";
+      }
+    }
+    else {
+      this.passwordBoolean = false;
+      if(this.password.invalid && this.retype.invalid)
+      {
+        this.errorMessage = "Both field is required"
+      }
+      else if(this.retype.invalid)
+      {
+        this.errorMessage = "Retype field is required"
+      }
+      else if(this.password.invalid){
+        this.errorMessage = "Password field is required";
+      }
+    }
+  }}
