b2a6edb77602adc62c7c1946da1f5c84d0837f04
JustinSoh <hoggersoh@gmail.com>
2018-07-16 08:01:57 +0800

    Created Backend API

---
+++ LogHubServer/LogHubWebAPI/LogHubWebAPI/Migrations/20180715235514_InitialCreate2.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace LogHubWebAPI.Migrations
+{
+    public partial class InitialCreate2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Users");
+        }
+    }
+}
---
+++ LogHubServer/LogHubWebAPI/LogHubWebAPI/Models/Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace LogHubWebAPI.Models
+{
+    public class Context : DbContext  
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
